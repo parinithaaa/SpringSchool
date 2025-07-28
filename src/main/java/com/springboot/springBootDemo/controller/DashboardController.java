b82c83194e9527cd45a070a066ff4c443d35b4f5
parinithaaa <parinitha463@gmail.com>
2025-07-28 22:24:27 +0530

    Login and Logout pages are configured.

---
+++ src/main/java/com/springboot/springBootDemo/controller/DashboardController.java
@@ -0,0 +1,22 @@
+package com.springboot.springBootDemo.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+	@Controller
+	public class DashboardController {
+
+	    @RequestMapping("/dashboard")
+	    public String displayDashboard(Model model,Authentication authentication) {
+	        model.addAttribute("username", authentication.getName());
+	        model.addAttribute("roles", authentication.getAuthorities().toString());
+	        return "dashboard.html";
+	    }
+}
+
+
